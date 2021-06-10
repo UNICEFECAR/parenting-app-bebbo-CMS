fab3be79a25c157812ae13a2c283b1eb63d916db
unknown <bharathi+1-at-817747646454>
2021-06-10 21:02:13 +0530

    API and migration Configuration

--- docroot/modules/custom/custom_serialization/src/Plugin/views/style/CustomSerializer.php
+++ docroot/modules/custom/custom_serialization/src/Plugin/views/style/CustomSerializer.php
@@ -5,6 +5,7 @@ use Drupal\rest\Plugin\views\style\Serializer;
 use Drupal\media\Entity\Media;
 use Drupal\file\Entity\File;
 use Drupal\taxonomy\Entity\Term;
+use Drupal\group\Entity\Group;
 
 /**
  * The style plugin for serialized output formats.
@@ -28,121 +29,165 @@ class CustomSerializer extends Serializer {
     $request_uri = \Drupal::service('path.current')->getPath(); //gives request path e.x (api/articles/en/1)
     $request = explode('/', $request_uri);
 
-    $array_of_multiple_values = ["child_age","keywords","related_articles","related_video_articles","related_activities"];
-    $media_fields = ["cover_image", "country_flag", "country_sponsor_logo", "country_national_partner", "cover_video"];    
-
-    $rows = array();
-    $data = array();
-    $url = '';
-    $mname = '';
-    $malt = '';
-    $site = '';
-    $thumbnail_url = '';    
+    //Validating request params to response error code
+    $validate_params_res = $this->check_request_params($request_uri);
+    if(empty($validate_params_res))
+    {
+      $array_of_multiple_values = ["child_age","keywords","related_articles","related_video_articles","related_activities"];
+      $media_fields = ["cover_image", "country_flag", "country_sponsor_logo", "country_national_partner", "cover_video"];    
+      
+      $rows = array();
+      $data = array();
+      $url = '';
+      $mname = '';
+      $malt = '';
+      $site = '';
+      $thumbnail_url = '';    
+      $field_formatter = array();    
+      if(isset($this->view->result) && !empty($this->view->result))
+      {    
+        foreach ($this->view->result as $row_index => $row) {
+          $this->view->row_index = $row_index;
     
-    $vocabulary_name = '';
-    if(isset($this->view->result) && !empty($this->view->result))
-    {    
-      foreach ($this->view->result as $row_index => $row) {
-        $this->view->row_index = $row_index;
-  
-        $view_render = $this->view->rowPlugin->render($row);
-        $view_render = json_encode($view_render);
-        $rendered_data = json_decode($view_render, true);   
-        $field_formatter = array();
-        foreach($rendered_data as $key => $values)
-        {          
-          //Custom image & video formattter
-          if (in_array($key,$media_fields)) //To check media image field exist
-          { 
-            if(!empty($values))
-            { 
-              $media_formatted_data = $this->custom_media_formatter($key, $values);   
-              $rendered_data[$key] = $media_formatted_data;
-            }
-          }
-          
-          //Custom array formatter
-          if (in_array($key,$array_of_multiple_values)) //To check mulitple field 
+          $view_render = $this->view->rowPlugin->render($row);
+          $view_render = json_encode($view_render);
+          $rendered_data = json_decode($view_render, true);   
+          foreach($rendered_data as $key => $values)
           {          
-            $array_formatted_data = $this->custom_array_formatter($values);   
-            $rendered_data[$key] = $array_formatted_data;
-          }   
-          
-          //Vocabularies Field formatter
-          if(strpos($request_uri, "vocabularies") !== false){            
-            if(!empty($values) && strpos($values, ',') !== false) // if the field have comma
+            //Custom image & video formattter
+            if (in_array($key,$media_fields)) //To check media image field exist
+            { 
+              if(!empty($values))
+              { 
+                $media_formatted_data = $this->custom_media_formatter($key, $values);   
+                $rendered_data[$key] = $media_formatted_data;
+              }
+            }
+            
+            //Custom array formatter
+            if (in_array($key,$array_of_multiple_values)) //To check mulitple field 
             {          
-              $formatted_data = explode(',',$values);
-              $field_formatter[$formatted_data[0]] = [
-                $key => html_entity_decode($formatted_data[1])
-              ];              
-            }  
-          }
+              $array_formatted_data = $this->custom_array_formatter($values);   
+              $rendered_data[$key] = $array_formatted_data;
+            }   
+            
+            //Custom Taxonomy Field Formatter
+            if(strpos($request_uri, "vocabularies") !== false || strpos($request_uri, "taxonomies") !== false){   
+              if(!empty($values) && strpos($values, ',') !== false) // if the field have comma
+              { 
+                $formatted_data = explode(',',$values);
+                $vocabulary_name = $formatted_data[1];
+                $vocabulary_machine_name = $formatted_data[0];
+                $taxonomy_data = $this->custom_taxonomy_field_formatter($request_uri, $key, $vocabulary_name, $vocabulary_machine_name);
+                $field_formatter[$formatted_data[0]] = $taxonomy_data;
+              }
+            }         
+          }                
           
-          //Taxonomies Field formatter
-          if(strpos($request_uri, "taxonomies") !== false){                      
-            if(!empty($values) && strpos($values, ',') !== false) // if the field have comma
-            {          
-              $term_data = [];
-              $formatted_data = explode(',',$values);
-              $vid = $formatted_data[0]; //vocabulary name
-              $terms =\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vid);
-              foreach ($terms as $term) {
-                $term_data[] = array(
-                  'id' => $term->tid,
-                  'name' => $term->name
-                );
-              }    
-              $field_formatter['vocabulary_name'] = html_entity_decode($formatted_data[1]);
-              $field_formatter[$formatted_data[0]] = $term_data;
-            }  
+          if(strpos($request_uri, "vocabularies") !== false || strpos($request_uri, "taxonomies") !== false){
+            $data = $field_formatter;
+          }        
+          else
+          {
+            $data[] = $rendered_data;
+            // To get total no of records
+            $rows['total'] = count($data);
           }
-        }                
-        
-        if(strpos($request_uri, "vocabularies") !== false || strpos($request_uri, "taxonomies") !== false){
-          $data[] = $field_formatter;
-        }        
-        else
+        }    
+    
+        // To validate request params
+        if(isset($request[3]) && !empty($request[3]))
         {
-          $data[] = $rendered_data;
-          // To get total no of records
-          $rows['total'] = count($data);
+          $rows['langcode'] = $request[3];
         }
-      }    
-  
-      // To validate request params
-      if(isset($request[3]) && !empty($request[3]))
-      {
-        $rows['langcode'] = $request[3];
-      }
-      if(isset($request[4]) && !empty($request[4]))
-      {
-        $rows['country'] = $request[4];
-      }
+        if(isset($request[4]) && !empty($request[4]))
+        {
+          $rows['country'] = $request[4];
+        }
 
-      $rows['data'] = $data;
-      unset($this->view->row_index);
-      if(strpos($request_uri, "taxonomies") !== false){
-       unset($rows['country']);
+        $rows['data'] = $data;
+        unset($this->view->row_index);
+        if(strpos($request_uri, "taxonomies") !== false){
+        unset($rows['country']);
+        }
+        // json output
+        if ((empty($this->view->live_preview))) {
+          $content_type = $this->displayHandler->getContentType();
+        }
+        else {
+          $content_type = !empty($this->options['formats']) ? reset($this->options['formats']) : 'json';
+        }
+        return $this->serializer->serialize($rows, $content_type, ['views_style_plugin' => $this]);
+      }    
+      else
+      {   
+        $rows = [];
+        $rows['error_code'] = 204;
+        $rows['message'] = "No Records Found";
+
+        return $this->serializer->serialize($rows, 'json', ['views_style_plugin' => $this]);
       }
-      // json output
-      if ((empty($this->view->live_preview))) {
-        $content_type = $this->displayHandler->getContentType();
-      }
-      else {
-        $content_type = !empty($this->options['formats']) ? reset($this->options['formats']) : 'json';
-      }
-      return $this->serializer->serialize($rows, $content_type, ['views_style_plugin' => $this]);
-    }    
+    }
     else
     {   
-      $rows = [];
-      $rows['message'] = "No Records Found";
-
-      return $this->serializer->serialize($rows, 'json', ['views_style_plugin' => $this]);
-    }
+      return $this->serializer->serialize($validate_params_res, 'json', ['views_style_plugin' => $this]);
+    }    
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  /*
+    To check request params is correct
+  */
+  public function check_request_params($request_uri)
+  {   
+    $request = explode('/', $request_uri);
+    
+    if(isset($request[3]) && !empty($request[3]))
+    {
+      $languages = \Drupal::languageManager()->getLanguages(); // get all enabled languages
+      $languages = json_encode($languages);
+      $languages = json_decode($languages, true);   
+      $languages_arr = array(); 
+      foreach($languages as $lang_code => $lang_name)
+      {
+        $languages_arr[] = $lang_code;
+      }
+      if(isset($languages_arr) && !empty($languages_arr))
+      {
+        if(!in_array($request[3],$languages_arr))
+        {
+          $respons_arr['error_code'] = 400;
+          $respons_arr['message'] = "Request language is wrong";
+
+          return $respons_arr;
+        }      
+      }
+    }
+    if(isset($request[4]) && !empty($request[4]))
+    {            
+      if(strpos($request_uri, "taxonomies") !== false){
+        return "";
+      }
+      else
+      {
+        $groups = Group::loadMultiple(); 	
+        foreach($groups as $gid => $group) {
+          $id = $group->get('id')->getString();    
+          $gids[] = $id;      
+        } 
+        if(!in_array($request[4],$gids))
+        {
+          $respons_arr['error_code'] = 400;
+          $respons_arr['message'] = "Request country code is wrong";
+
+          return $respons_arr;
+        } 
+      }      
+    }    
+    return "";  
+  }
 
   /**
    * {@inheritdoc}
@@ -253,4 +298,90 @@ class CustomSerializer extends Serializer {
     }            
     return $media_data;
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  /*
+    To get taxonomy terms details from db
+  */
+  public function custom_taxonomy_field_formatter($request_uri, $key, $vocabulary_name, $vocabulary_machine_name)
+  {
+    $taxonomy_vocabulary_machine_name = ["growth_period", "child_age", "growth_introductory", "standard_deviation"];
+    //Vocabularies Field formatter
+    if(strpos($request_uri, "vocabularies") !== false){                     
+      $vocabulary_data = [    
+        $key => html_entity_decode($vocabulary_name)
+      ];       
+      return $vocabulary_data;
+    }
+    
+    //Taxonomies Field formatter
+    if(strpos($request_uri, "taxonomies") !== false){                                     
+      $term_data = [];     
+      $terms =\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vocabulary_machine_name);
+      foreach ($terms as $term) {         
+        if($vocabulary_machine_name === "growth_period")
+        {
+          $term_obj = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($term->tid);                                    
+          $term_data[] = array(   
+            'id' => $term->tid,
+            'name' => $term->name,        
+            'vaccination_opens' => $term_obj->get('field_vaccination_opens')->value
+          );
+        }  
+        else if($vocabulary_machine_name === "child_age")
+        {
+          $term_obj = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($term->tid);                                    
+          $term_data[] = array(   
+            'id' => $term->tid,
+            'name' => $term->name,        
+            'days_from' => $term_obj->get('field_days_from')->value,
+            'days_to' => $term_obj->get('field_days_to')->value,
+            'buffers_days' => $term_obj->get('field_buffers_days')->value,
+            'age_bracket' => $term_obj->get('field_age_bracket')->target_id,
+            'weeks_from' => $term_obj->get('field_weeks_from')->value,
+            'weeks_to' => $term_obj->get('field_weeks_to')->value
+          );
+        }   
+        else if($vocabulary_machine_name === "growth_introductory")
+        {
+          $term_obj = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($term->tid);                                    
+          $term_data[] = array(   
+            'id' => $term->tid,
+            'name' => $term->name,        
+            'days_from' => $term_obj->get('field_days_from')->value,
+            'days_to' => $term_obj->get('field_days_to')->value
+          );
+        } 
+        else if($vocabulary_machine_name === "standard_deviation")
+        {
+          $term_obj = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($term->tid);                                    
+          $term_data[] = array(     
+            'id' => $term->tid,
+            'name' => $term->name,      
+            'child_gender' => $term_obj->get('field_child_gender')->target_id,
+            'growth_type' => $term_obj->get('field_growth_type')->target_id,
+            'sd0' => $term_obj->get('field_sd0')->value,
+            'sd1' => $term_obj->get('field_sd1')->value,
+            'sd2' => $term_obj->get('field_sd2')->value,
+            'sd3' => $term_obj->get('field_sd3')->value,
+            'sd4' => $term_obj->get('field_sd4')->value,
+            'sd1neg' => $term_obj->get('field_sd1neg')->value,
+            'sd2neg' => $term_obj->get('field_sd2neg')->value,
+            'sd3neg' => $term_obj->get('field_sd3neg')->value,
+            'sd4neg' => $term_obj->get('field_sd4neg')->value
+          );
+        }
+        else
+        {
+          $term_data[] = array(
+            'id' => $term->tid,
+            'name' => $term->name
+          ); 
+        }                   
+      }    
+      return $term_data;
+    }  
+  }  
 }
