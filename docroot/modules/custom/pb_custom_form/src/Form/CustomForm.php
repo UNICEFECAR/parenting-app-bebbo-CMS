c6cd5c72c9622e8c0973815325f26403259b597f
ponnammal <ponnammal@unimity.com-at-817747646454>
2021-08-31 22:59:24 +0530

    coder module code cleanup done

--- docroot/modules/custom/pb_custom_form/src/Form/CustomForm.php
+++ docroot/modules/custom/pb_custom_form/src/Form/CustomForm.php
@@ -1,8 +1,10 @@
 <?php
+
 /**
  * @file
  * Contains \Drupal\pb_custom_form\Form\CustomForm.
  */
+
 namespace Drupal\pb_custom_form\Form;
 
 use Drupal\Core\Form\FormBase;
@@ -15,13 +17,12 @@ use Drupal\group\Entity\Group;
 /**
  * Implements a Custom Form
  */
-class CustomForm extends FormBase
-{
+class CustomForm extends FormBase {
   /**
    * @return string
    */
-  public function getFormId()
-  {
+
+  public function getFormId() {
     return 'forcefull_check_update_api';
   }
 
@@ -30,39 +31,38 @@ class CustomForm extends FormBase
    * @param FormStateInterface $form_state
    * @return array
    */
-  public function buildForm(array $form, FormStateInterface $form_state)
-  {              
-    $groups = Group::loadMultiple(); 	
-    foreach($groups as $gid => $group) {
-      $id = $group->get('id')->getString(); 
-      $label = $group->get('label')->getString();   
-      $coutry_group[$id] = $label;      
-    } 
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $groups = Group::loadMultiple();
+    foreach ($groups as $gid => $group) {
+      $id = $group->get('id')->getString();
+      $label = $group->get('label')->getString();
+      $coutry_group[$id] = $label;
+    }
 
-    // Dropdown Select
+    /* Dropdown Select. */
     $form['country_select'] = [
       '#type' => 'select',
       '#title' => $this->t('Country'),
       '#options' => $coutry_group,
     ];
 
-    // CheckBoxes.
+    /* CheckBoxes. */
     $form['checkbox'] = [
-      '#type' => 'checkbox',      
+      '#type' => 'checkbox',
       '#title' => $this->t('Force Update Check'),
       '#return_value' => 1,
-      '#default_value' => FALSE
-    ];    
+      '#default_value' => FALSE,
+    ];
 
     $form['actions'] = [
       '#type' => 'actions',
     ];
 
-    // Add a submit button
+    /* Add a submit button. */
     $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Save'),
-      '#button_type' => 'primary'
+      '#button_type' => 'primary',
     ];
 
     return $form;
@@ -72,38 +72,34 @@ class CustomForm extends FormBase
    * @param array $form
    * @param FormStateInterface $form_state
    */
-  public function validateForm(array &$form, FormStateInterface $form_state)
-  {
-    // $title = $form_state->getValue('title');
-    // if (strlen($title) < 15) {
-    //   $form_state->setErrorByName('title', $this->t('The title must be at least 15 characters long.'));
-    // }
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    /* $title = $form_state->getValue('title');
+    if (strlen($title) < 15) {
+    $form_state->setErrorByName('title', $this->t('The title must be at least 15 characters long.'));
+    } */
   }
 
   /**
    * @param array $form
    * @param FormStateInterface $form_state
    */
-  public function submitForm(array &$form, FormStateInterface $form_state)
-  {
+  public function submitForm(array &$form, FormStateInterface $form_state) {
     $date = new DrupalDateTime();
     $conn = Database::getConnection();
     $conn->insert('forcefull_check_update_api')->fields(
-      array(
+      [
         'flag' => $form_state->getValue('checkbox'),
         'country_id' => $form_state->getValue('country_select'),
-        'updated_at' => $date->getTimestamp()
-      )
+        'updated_at' => $date->getTimestamp(),
+      ]
     )->execute();
 
     $checkbox = $form_state->getValue('checkbox');
     $text_msg = "Disabled";
-    if($checkbox == 1)
-    {
-        $text_msg = "Enbled";    
+    if ($checkbox == 1) {
+      $text_msg = "Enbled";
     }
-    
-    drupal_set_message("Force Check Update API ".$text_msg);
+    drupal_set_message("Force Check Update API " . $text_msg);
   }
 
 }
