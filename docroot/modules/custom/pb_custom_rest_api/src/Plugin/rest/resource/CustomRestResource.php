fb6c5b45dceb7c0a47d77423b1291754d417cfc4
unknown <bharathi+1-at-817747646454>
2021-09-03 15:39:46 +0530

    dashboard config changes and operations

--- docroot/modules/custom/pb_custom_rest_api/src/Plugin/rest/resource/CustomRestResource.php
+++ docroot/modules/custom/pb_custom_rest_api/src/Plugin/rest/resource/CustomRestResource.php
@@ -17,36 +17,32 @@ use Drupal\Core\Entity\Query\QueryFactory;
  *     "canonical" = "/api/check-update/{country}"
  *   }
  * )
- */ 
+ */
 
 class CustomRestResource extends ResourceBase {
-	/**
-	* Responds to GET requests.
-	*
-	* @param \Drupal\Core\Entity\EntityInterface $entity
-	*   The entity object.
-	*
-	* @return \Drupal\rest\ResourceResponse
-	*   The HTTP response object.
-	*
-	*/
-	public function get($country= null)
-	{
-		$database = \Drupal::database();
-		$query = $database->query("SELECT * FROM {forcefull_check_update_api} WHERE country_id = $country ORDER BY id DESC LIMIT 1");
-		$result = $query->fetchAll();
-		if(!empty($result))
-		{
-			$response_array['status'] = 200;
-			$response_array['flag'] = (int)$result[0]->flag;
-			$response_array['updated_at'] = $result[0]->updated_at;							
-		}													
-		else 
-		{
-			$response_array['status'] = 204;
-			$response_array['message'] = "No Records Found";
-		}		   	
-		return new ResourceResponse($response_array);
-	}  
+  /**
+   * Responds to GET requests.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity object.
+   *
+   * @return \Drupal\rest\ResourceResponse
+   *   The HTTP response object.
+   */
+  public function get ($country = NULL) {
+    $database = \Drupal::database();
+    $query = $database->query("SELECT * FROM {forcefull_check_update_api} WHERE country_id = $country ORDER BY id DESC LIMIT 1");
+    $result = $query->fetchAll();
+    if (!empty($result)) {
+      $response_array['status'] = 200;
+      $response_array['flag'] = (int) $result[0]->flag;
+      $response_array['updated_at'] = $result[0]->updated_at;
+    }
+    else {
+      $response_array['status'] = 204;
+      $response_array['message'] = "No Records Found";
+    }
+    return new ResourceResponse($response_array);
+  }
+
 }
-  
