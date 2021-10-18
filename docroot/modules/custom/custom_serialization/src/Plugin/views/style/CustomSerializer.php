031995850c18a5da855aa48fab9704fae52f1443
unknown <bharathi+1-at-817747646454>
2021-10-18 18:33:23 +0530

    removed empty p tag in description field

--- docroot/modules/custom/custom_serialization/src/Plugin/views/style/CustomSerializer.php
+++ docroot/modules/custom/custom_serialization/src/Plugin/views/style/CustomSerializer.php
@@ -116,6 +116,8 @@ class CustomSerializer extends Serializer {
               $body_summary = str_replace("\n", '', $body_summary);
               /* Remove span tag from body and summary field */
               $body_summary = preg_replace('/<span[^>]+\>|<\/span>/i', '', $body_summary);
+              /* Remove empty <p> </p> tag */
+              $body_summary = str_replace("<p> </p>", '', $body_summary);
               /* remove inline style attribute */
               $rendered_data[$key] = preg_replace('/(<[^>]*) style=("[^"]+"|\'[^\']+\')([^>]*>)/i', '$1$3', $body_summary);
             }
