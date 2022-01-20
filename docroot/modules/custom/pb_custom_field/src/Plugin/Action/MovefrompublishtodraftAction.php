559ebdc10c0b62bedaf607240ad70c8f834a8fea
ponnammal <ponnammal@unimity.com-at-817747646454>
2022-01-20 19:15:32 +0530

    Revert "T-89 bulk action code added"
    This reverts commit 1f7b4c866e4da191b571a9c55a755db14d698318.

--- docroot/modules/custom/pb_custom_field/src/Plugin/Action/MovefrompublishtodraftAction.php
+++ docroot/modules/custom/pb_custom_field/src/Plugin/Action/MovefrompublishtodraftAction.php
@@ -14,7 +14,7 @@ use Drupal\user\Entity\User;
  *
  * @Action(
  *   id = "pb_custom_field_publish_to_draft",
- *   label = @Translation("Change to Draft"),
+ *   label = @Translation("Publish to Archive then Draft"),
  *   type = "node",
  *   confirm = FALSE
  * )
@@ -27,7 +27,7 @@ class MovefrompublishtodraftAction extends ViewsBulkOperationsActionBase {
    *
    * @var int
    */
-  public $initial_count = 1;
+  public $initial = 0;
   /**
    * Get the total translated count.
    *
@@ -57,63 +57,122 @@ class MovefrompublishtodraftAction extends ViewsBulkOperationsActionBase {
    * {@inheritdoc}
    */
   public function execute(ContentEntityInterface $entity = NULL) {
-   $uid = \Drupal::currentUser()->id();
+    $uid = \Drupal::currentUser()->id();
     $user = User::load($uid);
-    $aaa = $this->initial_count++;
-    $aaab[] = $aaa;
-    $initial_countss = $this->initial_count + 1;
+
+    // $groups = array();
+    $grp_membership_service = \Drupal::service('group.membership_loader');
+    $grps = $grp_membership_service->loadByUser($user);
+    if (!empty($grps)) {
+      foreach ($grps as $grp) {
+        $groups = $grp->getGroup();
+      }
+      $grp_country_language = $groups->get('field_language')->getValue();
+      $grp_country_new_array = array_column($grp_country_language, 'value');
+    }
+
+    $this->initial = $this->initial + 1;
+    $this->processItem = $this->processItem + 1;
     $list = $this->context['list'];
-    $total = $this->context['sandbox']['total'];
-    $batch_size = $this->context['sandbox']['batch_size'];
-    $current_batch = $this->context['sandbox']['current_batch'];
-    $page = $this->context['sandbox']['page'];
     $list_count = count($list);
-    $rounds = $total/$batch_size;
     $message = "";
     $error_message = "";
-    $current_language[] = $entity->get('langcode')->value;   
-    foreach ($list as  $value) {
-     
-      $nids = $value[0];
-      $langs = $value[1];
-      $n_language[$nids][] = $value[1];
+    $current_language = $entity->get('langcode')->value;
+
+    $nid = $entity->get('nid')->getString();
+    $archive_node = node_load($nid);
+    $ids = array_column($list, '0');
+    $all_ids = implode(',', $ids);
+    $node_lang_archive = $archive_node->getTranslation($current_language);
+    $current_state = $node_lang_archive->moderation_state->value;
+
+    if ($current_state == 'published' && empty($grps)) {
+      /* Change status from publish to archive. */
+      $node_lang_archive->set('moderation_state', 'archive');
+      $node_lang_archive->set('uid', $uid);
+      $node_lang_archive->set('content_translation_source', $current_language);
+      $node_lang_archive->set('changed', time());
+      $node_lang_archive->set('created', time());
+
+      $uid = \Drupal::currentUser()->id();
+      $node_lang_archive->setNewRevision(TRUE);
+      $node_lang_archive->revision_log = 'Content changed  from “Published” to “Archive” and than “Draft”';
+      $node_lang_archive->setRevisionCreationTime(REQUEST_TIME);
+      $node_lang_archive->setRevisionUserId($uid);
+      $node_lang_archive->setRevisionTranslationAffected(NULL);
+      $node_lang_archive->save();
+      $archive_node->save();
+      /* Change status from publish to draft. */
+      $draft_node = Node::load($nid);
+      $node_lang_draft = $draft_node->getTranslation($current_language);
+      $node_lang_draft->set('moderation_state', 'draft');
+      $node_lang_draft->set('uid', $uid);
+      $node_lang_draft->set('content_translation_source', $current_language);
+      $node_lang_draft->set('changed', time());
+      $node_lang_draft->set('created', time());
+      $node_lang_draft->save();
+      $draft_node->save();
+      $this->assigned = $this->assigned + 1;
     }
-    
-    if ($aaa == 1 && $page == 0) {
+    elseif ($current_state == 'published' && !empty($grps)) {
+      if (in_array($current_language, $grp_country_new_array)) {
+        /* Change status from publish to archive. */
+        $node_lang_archive->set('moderation_state', 'archive');
+        $node_lang_archive->set('uid', $uid);
+        $node_lang_archive->set('content_translation_source', $current_language);
+        $node_lang_archive->set('changed', time());
+        $node_lang_archive->set('created', time());
 
-      $batch = [
-      'title' => t('change status'),
-      'operations' => [
-      [
-        '\Drupal\pb_custom_field\ChangeActionStatus::offLoadCountryProcessfordraft',
-        [$n_language,$all_nids]
-      ],
-      ],
-      'finished' => '\Drupal\pb_custom_field\ChangeActionStatus::offLoadsCountryProcessfordraftFinishedCallback',
-    ];
-    batch_set($batch);
-  }
+        $uid = \Drupal::currentUser()->id();
+        $node_lang_archive->setNewRevision(TRUE);
+        $node_lang_archive->revision_log = 'Content changed  from “Published” to “Archive” and than “Draft”';
+        $node_lang_archive->setRevisionCreationTime(REQUEST_TIME);
+        $node_lang_archive->setRevisionUserId($uid);
+        $node_lang_archive->setRevisionTranslationAffected(NULL);
+        $node_lang_archive->save();
+        $archive_node->save();
+        /* Change status from publish to draft. */
+        $draft_node = Node::load($nid);
+        $node_lang_draft = $draft_node->getTranslation($current_language);
+        $node_lang_draft->set('moderation_state', 'draft');
+        $node_lang_draft->set('uid', $uid);
+        $node_lang_draft->set('content_translation_source', $current_language);
+        $node_lang_draft->set('changed', time());
+        $node_lang_draft->set('created', time());
+        $node_lang_draft->save();
+        $draft_node->save();
+        $this->assigned = $this->assigned + 1;
+      }
+      else {
+        $this->countryRestrict = $this->countryRestrict + 1;
 
-    // if ($this->nonAssigned > 0) {
-    //   $error_message = $this->t("Please Select Published Content ( @nonassigned ) <br/>", ['@nonassigned' => $this->nonAssigned]);
-    // }
-    // else {
-    //   $message = $this->t("Content Changed Into Draft Successfully ( @assigned ) <br/>", ['@assigned' => $this->assigned]);
-    // }
+      }
+    }
+    else {
+      $this->nonAssigned = $this->nonAssigned + 1;
 
-    // if ($this->countryRestrict > 0) {
-    //   $error_message = $this->t("This content belongs to Master content and cannot be edited. It has to be assigned to your country to allow for further editing and contextualization. ( @countryRestrict ) <br/>", ['@countryRestrict' => $this->countryRestrict]);
-    // }
+    }
+
+    if ($this->nonAssigned > 0) {
+      $error_message = $this->t("Please Select Published Content ( @nonassigned ) <br/>", ['@nonassigned' => $this->nonAssigned]);
+    }
+    else {
+      $message = $this->t("Content Changed Into Draft Successfully ( @assigned ) <br/>", ['@assigned' => $this->assigned]);
+    }
+
+    if ($this->countryRestrict > 0) {
+      $error_message = $this->t("This content belongs to Master content and cannot be edited. It has to be assigned to your country to allow for further editing and contextualization. ( @countryRestrict ) <br/>", ['@countryRestrict' => $this->countryRestrict]);
+    }
 
     /* $message.="Please visit Country content page to view.";*/
-    // if ($list_count == $this->processItem) {
-    //   if (!empty($message)) {
-    //     drupal_set_message($message, 'status');
-    //   }
-    //   if (!empty($error_message)) {
-    //     drupal_set_message($error_message, 'error');
-    //   }
-    // }
+    if ($list_count == $this->processItem) {
+      if (!empty($message)) {
+        drupal_set_message($message, 'status');
+      }
+      if (!empty($error_message)) {
+        drupal_set_message($error_message, 'error');
+      }
+    }
 
     if ($this->initial == 1) {
       /* Please add the entity */
