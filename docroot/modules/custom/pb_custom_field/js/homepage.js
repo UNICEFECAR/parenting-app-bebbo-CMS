6a0493bb101006cd9c36a42b88c7e7c181c9d390
ponnammal <ponnammal.v@datamatics.com>
2022-02-08 16:30:01 +0530

    T-89 bulk & MObile share option & T-104 notification code added

--- docroot/modules/custom/pb_custom_field/js/homepage.js
+++ docroot/modules/custom/pb_custom_field/js/homepage.js
@@ -4,14 +4,14 @@ jQuery(document).ready(function() {
     });
   if(jQuery('input[name="field_make_available_for_mobile[value]"]').is(':checked'))
     {
-      jQuery( "<p>Please note this action can not be undone. Make sure you have request your IT team for backup of the database and dsiabled all Country users.</p>" ).insertAfter(".field--name-field-make-available-for-mobile");
+      jQuery( "<p>Please note this action can not be undone. Make sure you have requested your IT team for backup of the database and disabled all Country users.</p>" ).insertAfter(".field--name-field-make-available-for-mobile");
 }
 });
  // for offload country process js
  jQuery('.field--name-field-make-available-for-mobile').click(function() {
     if(jQuery('input[name="field_make_available_for_mobile[value]"]').is(':checked'))
     {
-      jQuery( "<p>Please note this action can not be undone. Make sure you have request your IT team for backup of the database and dsiabled all Country users.</p>" ).insertAfter(".field--name-field-make-available-for-mobile");
+      jQuery( "<p>Please note this action can not be undone. Make sure you have requested your IT team for backup of the database and disabled all Country users.</p>" ).insertAfter(".field--name-field-make-available-for-mobile");
 }else
 {
 jQuery('.field--name-field-make-available-for-mobile').nextAll('p').remove();     
