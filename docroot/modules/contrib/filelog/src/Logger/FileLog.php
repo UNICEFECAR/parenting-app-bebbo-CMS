350f6fbc1e7f87efae0656c7376704288d81ea88
bharathi.vediyam <bharathi@unimity.com>
2022-08-03 16:47:59 +0530

    bulk action logs changes

---
+++ docroot/modules/contrib/filelog/src/Logger/FileLog.php
@@ -0,0 +1,259 @@
+<?php
+
+namespace Drupal\filelog\Logger;
+
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Component\Render\PlainTextOutput;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\DependencyInjection\DependencySerializationTrait;
+use Drupal\Core\Logger\LogMessageParserInterface;
+use Drupal\Core\Logger\RfcLoggerTrait;
+use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\Core\State\StateInterface;
+use Drupal\Core\Utility\Token;
+use Drupal\filelog\FileLogException;
+use Drupal\filelog\LogFileManagerInterface;
+use Drupal\filelog\LogMessage;
+use Psr\Log\LoggerInterface;
+use function file_exists;
+use function fopen;
+use function fwrite;
+use function watchdog_exception;
+
+/**
+ * File-based logger.
+ */
+class FileLog implements LoggerInterface {
+
+  use RfcLoggerTrait;
+  use DependencySerializationTrait;
+
+  /**
+   * The filelog settings.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
+   * The state system, for updating the filelog.rotation timestamp.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * The token system, for formatting the log messages.
+   *
+   * @var \Drupal\Core\Utility\Token
+   */
+  protected $token;
+
+  /**
+   * The log message parser, for formatting the log messages.
+   *
+   * @var \Drupal\Core\Logger\LogMessageParserInterface
+   */
+  protected $parser;
+
+  /**
+   * The time system.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  protected $time;
+
+  /**
+   * The currently opened log file.
+   *
+   * @var resource
+   */
+  protected $logFile;
+
+  /**
+   * The STDERR fallback.
+   *
+   * @var resource
+   */
+  protected $stderr;
+
+  /**
+   * The log-file manager, providing file-handling methods.
+   *
+   * @var \Drupal\filelog\LogFileManagerInterface
+   */
+  protected $fileManager;
+
+  /**
+   * FileLog constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The config.factory service.
+   * @param \Drupal\Core\State\StateInterface $state
+   *   The state service.
+   * @param \Drupal\Core\Utility\Token $token
+   *   The token service.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The datetime.time service.
+   * @param \Drupal\Core\Logger\LogMessageParserInterface $parser
+   *   The logger.log_message_parser service.
+   * @param \Drupal\filelog\LogFileManagerInterface $fileManager
+   *   The filelog.file_manager service.
+   */
+  public function __construct(ConfigFactoryInterface $configFactory,
+                              StateInterface $state,
+                              Token $token,
+                              TimeInterface $time,
+                              LogMessageParserInterface $parser,
+                              LogFileManagerInterface $fileManager) {
+    $this->config = $configFactory->get('filelog.settings');
+    $this->state = $state;
+    $this->token = $token;
+    $this->time = $time;
+    $this->parser = $parser;
+    $this->fileManager = $fileManager;
+  }
+
+  /**
+   * Open the logfile for writing.
+   *
+   * @return bool
+   *   Returns TRUE if the log file is available for writing.
+   *
+   * @throws \Drupal\filelog\FileLogException
+   */
+  protected function openFile(): bool {
+    if ($this->logFile) {
+      return TRUE;
+    }
+
+    // When creating a new log file, save the creation timestamp.
+    $filename = $this->fileManager->getFileName();
+    $create = !file_exists($filename);
+    if (!$this->fileManager->ensurePath()) {
+      $this->logFile = $this->stderr();
+      throw new FileLogException('The log directory has disappeared.');
+    }
+    if ($this->logFile = fopen($filename, 'ab')) {
+      if ($create) {
+        $this->fileManager->setFilePermissions();
+        $this->state->set('filelog.rotation', $this->time->getRequestTime());
+      }
+      return TRUE;
+    }
+
+    // Log errors to STDERR until the end of the current request.
+    $this->logFile = $this->stderr();
+    throw new FileLogException('The logfile could not be opened for writing. Logging to STDERR.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function log($level, $message, array $context = []): void {
+    if (!$this->shouldLog($level, $message, $context)) {
+      return;
+    }
+
+    $entry = $this->render($level, $message, $context);
+
+    try {
+      $this->openFile();
+      $this->write($entry);
+    }
+    catch (FileLogException $error) {
+      // Log the exception, unless we were already logging a filelog error.
+      if ($context['channel'] !== 'filelog') {
+        watchdog_exception('filelog', $error);
+      }
+      // Write the message directly to STDERR.
+      fwrite($this->stderr(), $entry . "\n");
+    }
+  }
+
+  /**
+   * Decides whether a message should be logged or ignored.
+   *
+   * @param mixed $level
+   *   Severity level of the log message.
+   * @param string $message
+   *   Content of the log message.
+   * @param array $context
+   *   Context of the log message.
+   *
+   * @return bool
+   *   TRUE if the message should be logged, FALSE otherwise.
+   */
+  protected function shouldLog($level, string $message, array $context = []): bool {
+    // Ignore any messages below the configured severity.
+    // (Severity decreases with level.)
+    $should_log = $this->config->get('enabled') && $level <= $this->config->get('level');
+
+    // Include or exclude based on channel list.
+    return $should_log && (
+      ($this->config->get('channels_type') === 'include')
+      === in_array($context['channel'], $this->config->get('channels'), TRUE)
+    );
+  }
+
+  /**
+   * Renders a message to a string.
+   *
+   * @param mixed $level
+   *   Severity level of the log message.
+   * @param string $message
+   *   Content of the log message.
+   * @param array $context
+   *   Context of the log message.
+   *
+   * @return string
+   *   The formatted message.
+   */
+  protected function render($level, string $message, array $context = []): string {
+    // Populate the message placeholders.
+    $variables = $this->parser->parseMessagePlaceholders($message, $context);
+    // Pass in bubbleable metadata that are just discarded later to prevent a
+    // LogicException due to too early rendering. The metadata of the string
+    // is not needed as it is not used for cacheable output but for writing to a
+    // logfile.
+    $bubbleable_metadata_to_discard = new BubbleableMetadata();
+    $log = new LogMessage($level, $message, $variables, $context);
+    $entry = $this->token->replace(
+      $this->config->get('format'),
+      ['log' => $log],
+      [],
+      $bubbleable_metadata_to_discard
+    );
+    return PlainTextOutput::renderFromHtml($entry);
+  }
+
+  /**
+   * Open STDERR resource, or use STDERR constant if available.
+   *
+   * The STDERR constant is not defined in all PHP environments.
+   *
+   * @return resource
+   *   Reference to the STDERR stream resource.
+   */
+  protected function stderr() {
+    if ($this->stderr === NULL) {
+      $this->stderr = defined('STDERR') ? STDERR : fopen('php://stderr', 'wb');
+    }
+    return $this->stderr;
+  }
+
+  /**
+   * Write an entry to the logfile.
+   *
+   * @param string $entry
+   *   The value to write. This should contain no newline characters.
+   *
+   * @throws \Drupal\filelog\FileLogException
+   */
+  protected function write(string $entry): void {
+    if (!fwrite($this->logFile, $entry . "\n")) {
+      throw new FileLogException('The message could not be written to the logfile.');
+    }
+  }
+
+}
---
+++ docroot/modules/contrib/filelog/tests/src/Unit/FileLogTest.php
@@ -0,0 +1,246 @@
+<?php
+
+namespace Drupal\Tests\filelog\Unit;
+
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Component\FileSecurity\FileSecurity;
+use Drupal\Core\Logger\LogMessageParser;
+use Drupal\Core\State\StateInterface;
+use Drupal\Core\Utility\Token;
+use Drupal\filelog\LogFileManager;
+use Drupal\filelog\Logger\FileLog;
+use Drupal\filelog\LogMessage;
+use function file_get_contents;
+use function strtr;
+
+/**
+ * Test the file logger.
+ *
+ * @group filelog
+ */
+class FileLogTest extends FileLogTestBase {
+
+  /**
+   * A mock of the token service.
+   *
+   * @var \Drupal\Core\Utility\Token
+   */
+  protected $token;
+
+  /**
+   * The logger.log_message_parser service.
+   *
+   * @var \Drupal\Core\Logger\LogMessageParserInterface
+   */
+  protected $logMessageParser;
+
+  /**
+   * A mock of the datetime.time service.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  protected $time;
+
+  /**
+   * The virtual file-system.
+   *
+   * @var \org\bovigo\vfs\vfsStreamDirectory
+   */
+  protected $virtualFileSystem;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->token = $this->getMockBuilder(Token::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $this->token->method('replace')
+      ->willReturnCallback([static::class, 'tokenReplace']);
+
+    $this->time = $this->createMock(TimeInterface::class);
+    $this->time->method('getRequestTime')
+      ->willReturn($_SERVER['REQUEST_TIME']);
+
+    $this->logMessageParser = new LogMessageParser();
+
+  }
+
+  /**
+   * Test a particular configuration.
+   *
+   * Ensure that it logs the correct events.
+   *
+   * @param array $config
+   *   The filelog settings.
+   * @param array $events
+   *   The events to be logged.
+   * @param string $expected
+   *   The expected content of the log file after the test.
+   *
+   * @dataProvider providerFileLog
+   */
+  public function testFileLog(array $config, array $events, string $expected = ''): void {
+    /** @var \Drupal\Core\Config\ConfigFactoryInterface $configFactory */
+    $configFactory = $this->getConfigFactoryStub(
+      ['filelog.settings' => $config]
+    );
+
+    /** @var \Drupal\Core\State\StateInterface|\PHPUnit\Framework\MockObject\MockObject $state */
+    $state_data = ['filelog.rotation' => 0];
+    $state = $this->createMock(StateInterface::class);
+    $state->method('get')
+      ->willReturnCallback(
+        static function ($key) use (&$state_data) {
+          return $state_data[$key];
+        }
+      );
+    $state->method('set')
+      ->willReturnCallback(
+        static function ($key, $value) use (&$state_data) {
+          $state_data[$key] = $value;
+        }
+      );
+
+    $logger = new FileLog(
+      $configFactory,
+      $state,
+      $this->token,
+      $this->time,
+      $this->logMessageParser,
+      $logFileManager = new LogFileManager($configFactory, $this->fileSystem)
+    );
+
+    foreach ($events as $event) {
+      $logger->log($event['level'], $event['message'], $event['context']);
+    }
+
+    // The logger should ensure .htaccess whenever it opens the log file.
+    if ($expected) {
+      $logPath = $configFactory->get('filelog.settings')->get('location');
+      static::assertStringEqualsFile("$logPath/.htaccess", FileSecurity::htaccessLines(), '.htaccess file written correctly.');
+      static::assertEquals(0664, 0777 & fileperms($logFileManager->getFileName()), 'Logfile has 0664 permissions.');
+    }
+
+    // Read log output and remove file for the next test.
+    $content = '';
+    if ($this->virtualFileSystem->hasChild(LogFileManager::FILENAME)) {
+      $content = file_get_contents(
+        $this->virtualFileSystem->getChild(LogFileManager::FILENAME)->url()
+      );
+      $this->virtualFileSystem->removeChild(LogFileManager::FILENAME);
+    }
+
+    static::assertEquals($expected, $content);
+
+    // Check that the timestamp was saved if and only if a log was created.
+    $timestamp = $state->get('filelog.rotation');
+    static::assertEquals(
+      $content ? $_SERVER['REQUEST_TIME'] : 0,
+      $timestamp
+    );
+  }
+
+  /**
+   * Provide data for the level-checking test.
+   *
+   * @return array
+   *   All datasets for ::testFileLog().
+   */
+  public function providerFileLog(): array {
+    $config = [
+      'enabled'       => TRUE,
+      'location'      => 'vfs://filelog',
+      'level'         => 7,
+      'channels_type' => 'exclude',
+      'channels'      => [],
+      'format'        => '[log:level] [log:message]',
+    ];
+
+    $levels = LogMessage::getLevels();
+    $events = [];
+    $messages = [];
+    for ($i = 0; $i <= 7; $i++) {
+      $events[] = [
+        'level'   => $i,
+        'message' => "This is message @i.\n LD5}5>~\\8AiU * VH",
+        'context' => [
+          '@i'        => $i,
+          'timestamp' => 0,
+          'channel'   => "channel_$i",
+        ],
+      ];
+      $messages[] = $levels[$i] . " This is message $i.\\n LD5}5>~\\8AiU * VH";
+    }
+
+    $data = [];
+    for ($i = 0, $iMax = count($levels); $i <= $iMax; $i++) {
+      $expected = implode("\n", array_slice($messages, 0, $i + 1)) . "\n";
+      $data[$i] = [
+        'config'   => ['level' => $i] + $config,
+        'events'   => $events,
+        'expected' => $expected,
+      ];
+    }
+
+    $data[] = [
+      'config'   => ['enabled' => FALSE] + $config,
+      'events'   => $events,
+      'expected' => '',
+    ];
+
+    $data[] = [
+      'config'   => ['channels_type' => 'include'] + $config,
+      'events'   => $events,
+      'expected' => '',
+    ];
+
+    $data[] = [
+      'config'   => [
+        'channels_type' => 'include',
+        'channels'      => ['channel_3'],
+      ] + $config,
+      'events'   => $events,
+      'expected' => $messages[3] . "\n",
+    ];
+
+    $data[] = [
+      'config'   => ['channels' => ['channel_3']] + $config,
+      'events'   => $events,
+      'expected' => implode("\n",
+        array_slice($messages, 0, 3) + array_slice($messages, 4, 4, TRUE)
+      ) . "\n",
+    ];
+
+    return $data;
+  }
+
+  /**
+   * A very primitive mock for the token service.
+   *
+   * The full token integration is tested separately.
+   *
+   * @param string $text
+   *   The text to be replaced.
+   * @param array $data
+   *   The placeholders.
+   *
+   * @return string
+   *   The formatted text.
+   */
+  public static function tokenReplace(string $text, array $data): string {
+    /** @var \Drupal\filelog\LogMessage $message */
+    $message = $data['log'];
+    return strtr(
+      $text,
+      [
+        '[log:message]' => $message->getText(),
+        '[log:level]'   => $message->getLevel(),
+      ]
+    );
+  }
+
+}
