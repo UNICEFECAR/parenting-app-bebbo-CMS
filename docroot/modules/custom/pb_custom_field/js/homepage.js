73a17ddf895f5d108b6a8f5e722a28ea88dedaa1
akshaykamble270 <akshay.kamble@datamatics.com>
2023-08-24 20:06:54 +0530

    fixed scroll and header warning issue

--- docroot/modules/custom/pb_custom_field/js/homepage.js
+++ docroot/modules/custom/pb_custom_field/js/homepage.js
@@ -23,22 +23,56 @@ jQuery('.field--name-field-make-available-for-mobile').nextAll('p').remove();
 });
 
 
-jQuery(window).on('scroll', function() {
-  jQuery('.scroll-menu').each(function() {
-      if(jQuery(window).scrollTop() >= jQuery(this).position().top) {
-          var id = jQuery(this).attr('id');
-          jQuery('#block-mainnavigation .menu-item a').removeClass('is-active');
-          if (typeof id !==  "undefined") {
-            jQuery("#block-mainnavigation .menu-item").find('a[href="https://www.bebbo.app/#'+ id +'"]').addClass('is-active');
-          }
+jQuery(document).ready(function() {
+  var menuLinks = jQuery("#block-mainnavigation .menu-item a");
+  
+  var scrollOffset = -250;
+
+  jQuery(window).on('scroll', function() {
+    var currentScroll = jQuery(window).scrollTop();
+    
+    jQuery('.scroll-menu').each(function() {
+      if (currentScroll >= jQuery(this).position().top + scrollOffset) {
+        var id = jQuery(this).attr('id');
+        menuLinks.removeClass('is-active');
+        if (typeof id !== "undefined") {
+          var correspondingLink = menuLinks.filter('[href="https://staging.bebbo.app/#' + id + '"]');
+          correspondingLink.addClass('is-active');
+        }
       }
+    });
+  });
+
+  menuLinks.on('click', function() {
+    menuLinks.removeClass('is-active');
+    jQuery(this).addClass('is-active');
   });
 });
 
 jQuery(document).ready(function() {
+  var clickTimer = null;
+  var clickDelay = 10; // Milliseconds delay for double-click emulation
+
   jQuery("#block-mainnavigation .menu-item").on('click', 'a', function () {
-      var id = jQuery(this).attr('href');
-      jQuery('#block-mainnavigation li a.is-active').removeClass("is-active");
-    jQuery('#block-mainnavigation .menu-item').find('a[href="'+id+'"]').addClass('is-active');
+    var clickedElement = jQuery(this);
+
+    if (clickTimer === null) {
+      // First click
+      clickTimer = setTimeout(function() {
+        // Single click action
+        var id = clickedElement.attr('href');
+        jQuery('#block-mainnavigation li a.is-active').removeClass("is-active");
+        jQuery('#block-mainnavigation .menu-item').find('a[href="'+id+'"]').addClass('is-active');
+
+        clickTimer = null; // Reset the timer
+      }, clickDelay);
+    } else {
+      // Second click (double click)
+      clearTimeout(clickTimer);
+      clickTimer = null;
+
+      // Double click action
+      // Put your double-click behavior here
+    }
   });
 });
