d88f719eec8708c368381474708909a13ee7d845
akshaykamble270 <akshay.kamble@datamatics.com>
2023-11-27 11:53:31 +0530

    remove unwanted module

--- docroot/modules/contrib/tb_megamenu/webpack/webpack.dev.js
+++
@@ -1,8 +0,0 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-const plugins = require('./plugins');
-
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'source-map',
-});
