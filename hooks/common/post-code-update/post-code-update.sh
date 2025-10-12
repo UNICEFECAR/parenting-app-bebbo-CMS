d47fd1206dae680bdd76873b6589cece5769e1c2
shubhamsprasadqed42 <shubham.prasad@qed42.com>
2025-10-12 13:23:12 +0530

    BEBBO#42: Fixed deployment issue and integrated hooks for auto deployment.

---
+++ hooks/common/post-code-update/post-code-update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Cloud Hook: post-code-update
+#
+# The post-code-update hook runs in response to code commits.
+# When you push commits to a Git branch, the post-code-update hooks runs for
+# each environment that is currently running that branch. See
+# ../README.md for details.
+#
+# Usage: post-code-update site target-env source-branch deployed-tag repo-url
+#                         repo-type
+
+# Set the -e option to exit on failure.
+set -ev
+
+site="$1"
+target_env="$2"
+source_branch="$3"
+deployed_tag="$4"
+repo_url="$5"
+repo_type="$6"
+. `dirname $0`/../code-deploy.sh
---
+++ hooks/sites.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+# List of multiple sites present in the codebase.
+SITES=(default bangladesh ecuador pacific_islands somoa turkey zimbabwe)
