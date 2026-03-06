af870eb2f8fd1a6256b13ec43d1da55c5ab0e77b
Jaydev Bhatt <jaydev.bhatt@qed42.com>
2026-03-06 09:07:38 +0530

    Update settings files as per the split.

---
+++ docroot/sites/bangladesh/site.splits.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @file
+ * Includes site specific config splits.
+ */
+
+$config['config_split.config_split.bangladesh_site']['status'] = TRUE;
---
+++ docroot/sites/pacific_islands/site.splits.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @file
+ * Includes site specific config splits.
+ */
+
+$config['config_split.config_split.pacific_islands_site']['status'] = TRUE;
